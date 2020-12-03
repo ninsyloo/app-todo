bf2814d81bf50299982722c0d34770147c8d1345
ninsyloo <evelinareal@gmail.com>
2020-12-03 18:54:09 -0300

    update02

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,6 @@
 import React, {useState} from 'react'
 import TodoForm from './TodoForm'
 
-
 function Todo({todos, completeTodo, removeTodo, updateTodo}){
 
    const [edit, setEdit] = useState({
@@ -11,6 +10,7 @@ function Todo({todos, completeTodo, removeTodo, updateTodo}){
 
    //Vamos a crear el eventHandler de submitUpdate.
    
+   //En el map utilizamos el array de tareas (todos) que creamos con TodoList
    return todos.map((todo, index)=>(
 
         <div key={index}>
@@ -24,4 +24,11 @@ function Todo({todos, completeTodo, removeTodo, updateTodo}){
     ))
 }
 
+//todo.id representa el id que le generamos a cada input con el handleSubmit en todoFormjsx.
+//todo.text es el contenido que escribimos en el input
+//todavia falta agregarle funcionialidad a lost botones que creamos.
+
 export default Todo;
+
+//Acuerdense de importar Todo.jsx en TodoList.jsx para poder ir viendo los cambios
+//y dentro del return de la funcion TodoList agregar <Todo todos={todos}/>
